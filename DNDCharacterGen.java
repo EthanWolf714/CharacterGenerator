b855c3b0e371e0ccb9d5a5fe7f2f8bd844dab70c
Ethan <Emwolf16@yahoo.com>
2024-03-13 18:24:29 -0500

    Centered Button

--- DNDCharacterGen.java
+++ DNDCharacterGen.java
@@ -7,23 +7,26 @@ import java.util.Random;
 public class DNDCharacterGen extends JFrame{
     private JLabel characterLabel;
     private JButton generateBTN;
-    private DNDCharacterGen dndCharacter;
+    private CharacterGen characterGen;
 
 
     public DNDCharacterGen(){
 
         setTitle("D&D Character Generator");
-        setSize(300, 150);
+        setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+        setResizable(false);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2,1));
+        panel.setLayout(null);
+        
 
         characterLabel = new JLabel("", SwingConstants.CENTER);
         panel.add(characterLabel);
 
         generateBTN = new JButton("Generate Character");
+        generateBTN.setSize(160, 30);
+        generateBTN.setBounds(60, 60, 160, 30);
         generateBTN.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
@@ -35,7 +38,7 @@ public class DNDCharacterGen extends JFrame{
         add(panel);
         setVisible(true);
 
-        dndCharacter = new DNDCharacterGen();
+        
         
     }
 
