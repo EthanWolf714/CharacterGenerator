54fff2b46636854d1d13f9f2e26f67e1d0c1ecd7
Ethan <Emwolf16@yahoo.com>
2024-03-15 21:40:21 -0500

    subClass and subRace implementation

--- CharacterGen.java
+++ CharacterGen.java
@@ -2,13 +2,16 @@ import java.util.Random;
 
 public class CharacterGen{
     private String[] classes = {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
+    private String[] subClasses;
     private String[] races = {"Dragonborn", "Dwarf", "Elf", "Gnome", "Half-elf", "Halfling", " Half-Orc", "Human", "Tiefling"};
+    private String[] subRaces;
 
     public String  generateCharacter(){
         Random random = new Random();
 
         int raceIndex = random.nextInt(races.length);
         String race = races[raceIndex];
+
 
 
         int classIndex = random.nextInt(classes.length);
