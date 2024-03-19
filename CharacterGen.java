5557aed3f796f8ff83d4f37d0a17444ee22b4a58
Ethan <Emwolf16@yahoo.com>
2024-03-19 13:14:15 -0500

    DiceRoller class logic

--- CharacterGen.java
+++ CharacterGen.java
@@ -1,10 +1,13 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class CharacterGen{
     private String[] classes = {"Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
     private String[] races = {"Dragonborn", "Dwarf", "Elf", "Gnome", "Half-elf", "Halfling", " Half-Orc", "Human", "Tiefling"};
-    private int[] stats = {};
+
     public String  generateCharacter(){
+        DiceRoller charStats = new DiceRoller();
+        int[] stats = charStats.rollStats();
         Random random = new Random();
 
         int raceIndex = random.nextInt(races.length);
@@ -19,7 +22,8 @@ public class CharacterGen{
         
         
 
-        String characterDescription = "<html>" + race + "<br>" + characterClass + "</html>";
+        String characterDescription = "<html>" +"Race: "+ race + "<br>" +"Class: " + characterClass + 
+        "<br>" + Arrays.toString(stats) +"</html>";
         return characterDescription;
     }
 
--- DiceRoller.java
+++ DiceRoller.java
@@ -1,11 +1,35 @@
 import java.util.Arrays;
 import java.util.Random;
 public class DiceRoller {
-    public static int[] rollDice(){
-        Random random = new Random();
-        int[] rolls = new int[6];
-        
+    private Random random;
 
-        return rolls;
+    public DiceRoller(){
+        random = new Random();
     }
+
+    public int[] rollStats(){
+        int[] stats = new int[6];
+        for(int i = 0; i < 6; i++){
+            stats[i] = rollSingeStat();
+        }
+        return stats;
+
+    }
+
+    private int rollSingeStat(){
+        int[] dice = new int[4];
+
+        for(int i = 0; i < 4; i++){
+            dice[i] = random.nextInt(6) + 1;
+
+        }
+        Arrays.sort(dice);
+
+        int sum = 0;
+        for(int i = 1; i < 4; i++){
+            sum += dice[i];
+        }
+        return sum;
+    }
+    
 }
