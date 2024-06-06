c23804237272b3d0948b75fc5b93618c5697eefb
Ethan <Emwolf16@yahoo.com>
2024-06-06 17:40:03 -0500

    got subraces working

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 
 
-window.onload = function(){
+window.onload = function () {
     populateSelectBox();
-    subRace();
+    subRaceOptions();
 }
-var classes = ['Artifcier','Barbarian', 'Bard', 'Cleric','Druid', 'Fighter','Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorccerer', 'Warlock', 'Wizard'];
-var races = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome','Half-elf', 'Halfings','Half-orc','Human','Tiefling'];
-var alignments = ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Nuetral', 'Chaotic Neutral', 'Lawful Evil','Neutral Evil','Chaotic Evil'];
-var backgrounds = ['Acoltye','Charlatan','Criminal', 'Entertainer','Folk hero','Guild Artisan','Hermit','Knight','Noble','Outlander', 'Pirate','Sage','Sailor', 'Soldier','Urchin'];
+var classes = ['Artifcier', 'Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorccerer', 'Warlock', 'Wizard'];
+var races = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-elf', 'Halfings', 'Half-orc', 'Human', 'Tiefling'];
+var alignments = ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Nuetral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil'];
+var backgrounds = ['Acoltye', 'Charlatan', 'Criminal', 'Entertainer', 'Folk hero', 'Guild Artisan', 'Hermit', 'Knight', 'Noble', 'Outlander', 'Pirate', 'Sage', 'Sailor', 'Soldier', 'Urchin'];
 
-function populateSelectBox(){
+function populateSelectBox() {
     var selectClass = document.getElementById('inputClass');
     var selectRace = document.getElementById('inputRace');
     var selectAlign = document.getElementById('inputAlign');
     var selectBackground = document.getElementById('inputBackground');
-    for(var i = 0; i < classes.length; i++){
-        var  opt = classes[i];
+    for (var i = 0; i < classes.length; i++) {
+        var opt = classes[i];
         var el = document.createElement("option");
         el.textContent = opt;
         el.value = opt;
         selectClass.appendChild(el);
     }
 
-    for(var i = 0; i < races.length; i++){
+    for (var i = 0; i < races.length; i++) {
         var opt = races[i];
         var el = document.createElement("option");
         el.textContent = opt;
@@ -30,7 +30,7 @@ function populateSelectBox(){
         selectRace.appendChild(el);
     }
 
-    for(var i = 0; i < alignments.length; i++){
+    for (var i = 0; i < alignments.length; i++) {
         var opt = alignments[i];
         var el = document.createElement("option");
         el.textContent = opt;
@@ -38,29 +38,29 @@ function populateSelectBox(){
         selectAlign.appendChild(el);
     }
 
-    for(var i = 0; i < backgrounds.length; i++){
+    for (var i = 0; i < backgrounds.length; i++) {
         var opt = backgrounds[i];
         var el = document.createElement("option");
         el.textContent = opt;
         el.value = opt;
         selectBackground.appendChild(el);
     }
-    
+
 }
 
 
 
 
-function randomStat(){
+function randomStat() {
     var stat = [];
-    for(var i = 0; i < 4; i++){
-        stat.push(Math.floor(Math.random()* 6) + 1);
+    for (var i = 0; i < 4; i++) {
+        stat.push(Math.floor(Math.random() * 6) + 1);
     }
-    stat.sort((a,b) => b - a);
+    stat.sort((a, b) => b - a);
     return stat[0] + stat[1] + stat[3];
 }
 
-function genStats(){
+function genStats() {
     document.getElementById('str').innerText = randomStat();
     document.getElementById('dex').innerText = randomStat();
     document.getElementById('con').innerText = randomStat();
@@ -74,21 +74,43 @@ function genStats(){
 
 function subRaceOptions() {
     const race = document.getElementById('inputRace').value;
-    const subRace = document.getElementById('inputSubrace');
-    
+    const subraceSelect = document.getElementById('inputSubrace');
+    subraceSelect.innerHTML = '';
 
     const subraces = {
         'Dragonborn': [],
         'Dwarf': ['Hill Dwarf', 'Mountain Dwarf'],
-        'Elf': ['Dark-Elf(Drow)', 'High-elf','Wood-elf'],
+        'Elf': ['Dark-Elf(Drow)', 'High-elf', 'Wood-elf'],
         'Gnome': ['Forest Gnome', 'Deep Gnome'],
         'Half-elf': [],
         'Halfings': ['Lightfoot Halfling', 'Stout Halfling'],
         'Half-orc': [],
         'Human': [],
         'Tiefling': [],
+    };
+
+    if (subraces[race]) {
+        subraces[race].forEach(subrace => {
+            const option = document.createElement('option');
+            option.value = subrace;
+            option.text = subrace;
+            subraceSelect.appendChild(option);
+        });
+    } else {
+        const defaultOption = document.createElement("option");
+        defaultOption.value = '';
+        defaultOption.text = "No subrace available";
+        subraceSelect.appendChild(defaultOption);
+
     }
-} 
-function subClass(){
+}
+function subClassOptions() {
+    const classSelect = document.getElementById('inputClass');
+    const subClassSelect = document.getElementById('inputSubclass')
+    subClassSelect.innerHTML = '';
+
+    
+    
+    
 
 }
