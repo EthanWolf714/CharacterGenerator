4060f6929c24173ff1001fc0130a59cca6081082
Ethan <Emwolf16@yahoo.com>
2024-06-17 14:29:21 -0500

    set up local storage retrival on results.html

--- index.js
+++ index.js
@@ -143,3 +143,46 @@ function subClassOptions() {
     
 
 }
+
+document.getElementById('character-form').addEventListener('submit',function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('inputName').value;
+    const charClass = document.getElementById('inputSubclass').value;
+    const subClass = document.getElementById('inputClass').value;
+    const background = document.getElementById('inputBackground').value;
+    const race = document.getElementById('inputRace').value;
+    const subRace = document.getElementById('inputSubrace').value;
+    const align = document.getElementById('inputAlign').value;
+    const str = document.getElementById('str').innerText;
+    const dex = document.getElementById('dex').innerText;
+    const con = document.getElementById('con').innerText;
+    const int = document.getElementById('int').innerText;
+    const wis = document.getElementById('wis').innerText;
+    const cha = document.getElementById('cha').innerText;
+
+
+
+    const character = {
+        name: name,
+        class: charClass,
+        subclass: subClass,
+        background: background,
+        race: race,
+        subrace: subRace,
+        alignment: align,
+        stats:{
+            str: str,
+                    dex: dex,
+                    con: con,
+                    int: int,
+                    wis: wis,
+                    cha: cha
+        }
+
+
+    };
+    localStorage.setItem('character', JSON.stringify(character));
+
+    window.location.href = 'results.html';
+});
--- results.js
+++ results.js
@@ -1,42 +1,21 @@
-document.getElementById('character-form').addEventListener('submit',function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('inputName').value;
-    const charClass = document.getElementById('inputSubclass').value;
-    const subClass = document.getElementById('inputClass').value;
-    const background = document.getElementById('inputBackground').value;
-    const race = document.getElementById('inputRace').value;
-    const subRace = document.getElementById('inputSubrace').value;
-    const align = document.getElementById('inputAlign').value;
-    const str = document.getElementById('str').innerText;
-    const dex = document.getElementById('dex').innerText;
-    const con = document.getElementById('con').innerText;
-    const int = document.getElementById('int').innerText;
-    const wis = document.getElementById('wis').innerText;
-    const cha = document.getElementById('cha').innerText;
-
-
-
-    const character = {
-        name: name,
-        class: charClass,
-        subclass: subClass,
-        background: background,
-        race: race,
-        subrace: subRace,
-        alignment: align,
-        stats:{
-            str: str,
-                    dex: dex,
-                    con: con,
-                    int: int,
-                    wis: wis,
-                    cha: cha
-        }
-
-
-    };
-    localStorage.setItem('character', JSON.stringify(character));
-
-    window.location.href = 'results.html';
+document.addEventListener('DOMContentLoaded', function(){
+    const character = JSON.parse(localStorage.getItem('character'));
+    if(character){
+        document.getElementById('charName').innerText = character.name;
+        document.getElementById('charClass').innerText = character.class;
+        document.getElementById('charSubclass').innerText = character.subclass;
+        document.getElementById('charRace').innerText = character.race;
+        document.getElementById('charSubrace').innerText = character.subrace;
+        document.getElementById('charBackground').innerText = character.background;
+        document.getElementById('charAlign').innerText = character.alignment;
+        document.getElementById('charStr').innerText = character.str;
+        document.getElementById('charDex').innerText = character.dex;
+        document.getElementById('charCon').innerText = character.con;
+        document.getElementById('charInt').innerText = character.int;
+        document.getElementById('charWis').innerText = character.wis;
+        document.getElementById('charCha').innerText = character.cha;
+        
+    }else{
+        window.location.href = 'index.html';
+    }
 });
