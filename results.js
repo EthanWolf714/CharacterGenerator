4f8bdbb31cf8be822cf9ae1c6fb9e98f47a71ff7
Ethan <Emwolf16@yahoo.com>
2024-06-26 19:58:16 -0500

    testing displayCharacter function

--- results.js
+++ results.js
@@ -1,3 +1,15 @@
-function displayResults(){
+function displayCharacter(){
+    const character = JSON.parse(localStorage.getItem('charcter'));
+    if(character){
+        const characterInfo = `
+        <p><strong>Name:</strong> ${character.name}</p>
+         <p><strong>Class:</strong> ${character.Class}</p>
+          <p>Subclass: ${character.subclass}</p>
+        
+        
+        `;
+    }else{
+
+    }
     
 }
